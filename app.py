1deccb470df944dd1a56ff57fdca59975c31d1ab
Göktuğ Türkdağ <goktugturkdag@proton.me>
2025-12-09 21:26:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ import io
 from langchain_community.vectorstores import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.chains import ConversationalRetrievalChain
+from langchain import ConversationalRetrievalChain
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.schema import Document, StrOutputParser # Creative Corner için
 from langchain.prompts import PromptTemplate, ChatPromptTemplate # Creative Corner için
